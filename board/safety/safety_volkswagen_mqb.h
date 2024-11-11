e2883d09534c6c48e8f00775e724979ad4e3ce7c
Szymon Acedański <accek@mimuw.edu.pl>
2024-11-11 16:35:04 +0100

    debug

--- board/safety/safety_volkswagen_mqb.h
+++ board/safety/safety_volkswagen_mqb.h
@@ -212,7 +212,8 @@ static void volkswagen_mqb_rx_hook(const CANPacket_t *to_push) {
       if (volkswagen_longitudinal) {
         bool set_button = GET_BIT(to_push, 16U);
         bool resume_button = GET_BIT(to_push, 19U);
-        if ((volkswagen_set_button_prev && !set_button) || (volkswagen_resume_button_prev && !resume_button)) {
+        //if ((volkswagen_set_button_prev && !set_button) || (volkswagen_resume_button_prev && !resume_button)) {
+        if (set_button || resume_button) {
           controls_allowed = acc_main_on;
           controls_allowed_long = acc_main_on;
         }
